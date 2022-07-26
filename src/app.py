f8f1157f77ff25bd7c1c4c6e847f700379f89969
BenjaminNassW <benjanass@gmail.com>
2022-07-26 22:48:26 +0000

    buscar por id

--- src/app.py
+++ src/app.py
@@ -16,14 +16,19 @@ CORS(app)
 jackson_family = FamilyStructure("Jackson")
 
 # Handle/serialize errors like a JSON object
+
+
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
+
+
 @app.route('/')
 def sitemap():
     return generate_sitemap(app)
+
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
@@ -35,8 +40,19 @@ def handle_hello():
         "family": members
     }
 
-
     return jsonify(response_body), 200
+
+
+@app.route('/members/<int:id>', methods=['GET'])
+def handle_member_id(id):
+
+    member = jackson_family.get_member(id)
+    if member:
+
+        response_body = member
+
+        return jsonify(response_body), 200
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
--- src/datastructures.py
+++ src/datastructures.py
@@ -49,9 +49,11 @@ class FamilyStructure:
         pass
 
     def get_member(self, id):
-        # fill this method and update the return
-        pass
+        for member in self._members:
+            if (member["id"] == id):
+                return member
 
     # this method is done, it returns a list with all the family members
+
     def get_all_members(self):
         return self._members
