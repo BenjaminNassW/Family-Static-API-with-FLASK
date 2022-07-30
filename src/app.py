1d45263d9ca5ddc9d826022b55837717eced245e
BenjaminNassW <benjanass@gmail.com>
2022-07-30 16:39:33 +0000

    error codes ready

--- src/app.py
+++ src/app.py
@@ -32,14 +32,16 @@ def sitemap():
 
 @app.route('/members', methods=['GET'])
 def handle_hello():
-
-    # this is how you can use the Family datastructure by calling its methods
     members = jackson_family.get_all_members()
     response_body = {
         "family": members
     }
-
-    return jsonify(response_body), 200
+    if response_body:
+        return jsonify(response_body), 200
+    if response_body is None:
+        return jsonify({"Message": "List is empty"}), 400
+    else:
+        return jsonify({"Message": "Server error"}), 500
 
 
 @app.route('/members/<int:id>', methods=['GET'])
@@ -47,23 +49,35 @@ def handle_member_id(id):
 
     member = jackson_family.get_member(id)
     if member:
-
         response_body = member
-
         return jsonify(response_body), 200
+    if member is None:
+        return jsonify("incorrect ID"), 400
+    else:
+        return jsonify("server error"), 500
 
 
 @app.route('/member', methods=['POST'])
 def handle_new_member():
     member = request.get_json()
-    jackson_family.add_member(member)
-    return jsonify(), 200
+    if member["first_name"] is None or member["last_name"] is None or member["age"] is None or member["lucky numbers"] is None:
+        return jsonify({"Message": "Incorrect information"}), 400
+    if member:
+        jackson_family.add_member(member)
+        return jsonify({"Message": "Member created"}), 200
+    else:
+        return jsonify({"Message": "Server error"}), 500
 
 
 @app.route('/member/<int:member_id>', methods=['DELETE'])
 def handle_delete_member(member_id):
     resultado = jackson_family.delete_member(member_id)
-    return jsonify({"mensaje": resultado}), 200
+    if resultado == "eliminado":
+        return jsonify({"Message": "Member deleted"}), 200
+    if resultado == "no se encontro el elemento":
+        return jsonify({"Message": "Member not found"}), 400
+    else:
+        return jsonify(), 500
 
 
     # this only runs if `$ python src/app.py` is executed
